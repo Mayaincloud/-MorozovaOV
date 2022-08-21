90656d64b80833983db8a96595394c4aac3b528e
Olga M <89043833391@mail.ru>
2022-08-21 18:12:15 +0500

    Seminar4_Homework_Task2

---
+++ Seminar4_Homework/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int num = userNumber;
+
+while(num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+    
+}
+
+Console.WriteLine($"Сумма всех цифр числа {userNumber} равна {sum}");
