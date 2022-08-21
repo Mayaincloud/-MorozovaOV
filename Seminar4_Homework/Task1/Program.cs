d05dbb7099ab6d224320c192c61b990b1db4bfa1
Olga M <89043833391@mail.ru>
2022-08-21 17:59:01 +0500

    Seminar4_Homework_Task1

---
+++ Seminar4_Homework/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+//Нужно создать цикл, который будет умножать число А на себя В раз
+
+Console.WriteLine("Введите первое число:");
+int UserNumber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int UserNumber2 = Convert.ToInt32(Console.ReadLine());
+
+int UserNumberDegree = UserNumber1;
+
+for(int i = 1; i < UserNumber2; i++)
+{
+    UserNumberDegree = UserNumberDegree * UserNumber1;
+ 
+}
+
+Console.WriteLine($"{UserNumber1} в степени {UserNumber2} равно {UserNumberDegree}");
