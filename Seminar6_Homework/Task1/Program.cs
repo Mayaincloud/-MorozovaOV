88969a6958a388be51a609fea5a757279b92b357
Olga M <89043833391@mail.ru>
2022-08-28 21:38:31 +0500

    Seminar6_Homework\Task1

---
+++ Seminar6_Homework/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write($"Какое количество чисел вы хотите ввести?: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] userArray = new int[m];
+
+ int [] InputArray(int m)
+{
+   
+    for(int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите число от -100 до 100: ");
+        userArray[i] = Convert.ToInt32(Console.ReadLine());
+        
+       
+    }
+        return userArray;
+}
+InputArray(m);
+
+
+
+    int j = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] > 0) j += 1;
+    }
+    
+
+Console.WriteLine($"Введено чисел больше 0: {j} ");
